cf69c801a6b1315faca30259bbe0a074a4ad318b
Evelin Amorim <evelin.f.amorim@inescted.pt>
2025-03-14 12:49:25 +0000

    adding again some methods

--- static/scripts.js
+++ static/scripts.js
@@ -520,4 +520,11 @@ function createFragments(text, sentence) {
     return fragments;
 }
 
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
 
